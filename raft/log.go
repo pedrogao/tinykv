159403f5ad37b38f6c3093b0ef74d2c8279e68b5
pedrogao <1312342604@qq.com>
2022-04-16 15:32:03 +0800

    feat: update docs

--- raft/log.go
+++ raft/log.go
@@ -14,7 +14,10 @@
 
 package raft
 
-import pb "github.com/pingcap-incubator/tinykv/proto/pkg/eraftpb"
+import (
+	"fmt"
+	pb "github.com/pingcap-incubator/tinykv/proto/pkg/eraftpb"
+)
 
 // RaftLog manage the log entries, its struct look like:
 //
@@ -30,23 +33,28 @@ type RaftLog struct {
 
 	// committed is the highest log position that is known to be in
 	// stable storage on a quorum of nodes.
+	// 已提交
 	committed uint64
 
 	// applied is the highest log position that the application has
 	// been instructed to apply to its state machine.
 	// Invariant: applied <= committed
+	// 已应用
 	applied uint64
 
 	// log entries with index <= stabled are persisted to storage.
 	// It is used to record the logs that are not persisted by storage yet.
 	// Everytime handling `Ready`, the unstabled logs will be included.
+	// 已持久化序号
 	stabled uint64
 
 	// all entries that have not yet compact.
+	// 日志项
 	entries []pb.Entry
 
 	// the incoming unstable snapshot, if any.
 	// (Used in 2C)
+	// 快照
 	pendingSnapshot *pb.Snapshot
 
 	// Your Data Here (2A).
@@ -56,7 +64,14 @@ type RaftLog struct {
 // to the state that it just commits and applies the latest snapshot.
 func newLog(storage Storage) *RaftLog {
 	// Your Code Here (2A).
-	return nil
+	return &RaftLog{
+		storage:         storage,
+		committed:       0,
+		applied:         0,
+		stabled:         0,
+		entries:         make([]pb.Entry, 0),
+		pendingSnapshot: nil,
+	}
 }
 
 // We need to compact the log entries in some point of time like
@@ -69,23 +84,30 @@ func (l *RaftLog) maybeCompact() {
 // unstableEntries return all the unstable entries
 func (l *RaftLog) unstableEntries() []pb.Entry {
 	// Your Code Here (2A).
-	return nil
+	return l.entries
 }
 
 // nextEnts returns all the committed but not applied entries
 func (l *RaftLog) nextEnts() (ents []pb.Entry) {
 	// Your Code Here (2A).
-	return nil
+	// commited but not applied
+	cGap := l.committed - l.stabled
+	aGap := l.applied - l.stabled
+	return l.entries[cGap:aGap]
 }
 
 // LastIndex return the last index of the log entries
 func (l *RaftLog) LastIndex() uint64 {
 	// Your Code Here (2A).
-	return 0
+	return l.applied + uint64(len(l.entries))
 }
 
 // Term return the term of the entry in the given index
 func (l *RaftLog) Term(i uint64) (uint64, error) {
 	// Your Code Here (2A).
-	return 0, nil
+	if i < l.stabled {
+		return 0, fmt.Errorf("input index: %d can't small than stable: %d", i, l.stabled)
+	}
+	gap := i - l.stabled
+	return l.entries[gap].Term, nil
 }
--- raft/raft.go
+++ raft/raft.go
@@ -104,14 +104,14 @@ func (c *Config) validate() error {
 // Progress represents a follower’s progress in the view of the leader. Leader maintains
 // progresses of all followers, and sends entries to the follower based on its progress.
 type Progress struct {
-	Match, Next uint64
+	Match, Next uint64 // 匹配日志序号，下一个日志序号
 }
 
 type Raft struct {
 	id uint64
 
-	Term uint64
-	Vote uint64
+	Term uint64 // 任期
+	Vote uint64 // 选票
 
 	// the log
 	RaftLog *RaftLog
@@ -123,7 +123,7 @@ type Raft struct {
 	State StateType
 
 	// votes records
-	votes map[uint64]bool
+	votes map[uint64]bool // 投票记录
 
 	// msgs need to send
 	msgs []pb.Message
@@ -137,16 +137,19 @@ type Raft struct {
 	electionTimeout int
 	// number of ticks since it reached last heartbeatTimeout.
 	// only leader keeps heartbeatElapsed.
+	// 距离上一次心跳超时过去了多久，leader 独有
 	heartbeatElapsed int
 	// Ticks since it reached last electionTimeout when it is leader or candidate.
 	// Number of ticks since it reached last electionTimeout or received a
 	// valid message from current leader when it is a follower.
+	// 距离上一次选举超时过去了多久，leader，candidate 独有
 	electionElapsed int
 
 	// leadTransferee is id of the leader transfer target when its value is not zero.
 	// Follow the procedure defined in section 3.10 of Raft phd thesis.
 	// (https://web.stanford.edu/~ouster/cgi-bin/papers/OngaroPhD.pdf)
 	// (Used in 3A leader transfer)
+	// leader id 变更
 	leadTransferee uint64
 
 	// Only one conf change may be pending (in the log, but not yet
@@ -156,7 +159,11 @@ type Raft struct {
 	// be proposed if the leader's applied index is greater than this
 	// value.
 	// (Used in 3A conf change)
+	// 配置变更
 	PendingConfIndex uint64
+
+	// 节点配置
+	config *Config
 }
 
 // newRaft return a raft peer with the given config
@@ -165,7 +172,27 @@ func newRaft(c *Config) *Raft {
 		panic(err.Error())
 	}
 	// Your Code Here (2A).
-	return nil
+	// If you need to send out a message, just push it to raft.Raft.msgs
+	// and all messages the raft received will be passed to raft.Raft.Step().
+	// 如果需要发送消息，那么只需要 push 到 msgs 中，消息接收后，会被 Step 函数回调
+	return &Raft{
+		id:               c.ID,
+		Term:             0, // 任期从0开始
+		Vote:             0, // 不存在 id 为 0的节点，因此 0表示未投票
+		RaftLog:          newLog(c.Storage),
+		Prs:              nil,
+		State:            StateFollower, // 超时后成为 candidate 发起投票
+		votes:            map[uint64]bool{},
+		msgs:             make([]pb.Message, 0),
+		Lead:             0, // 0 表示无
+		heartbeatTimeout: c.HeartbeatTick,
+		electionTimeout:  c.ElectionTick,
+		heartbeatElapsed: 0,
+		electionElapsed:  0,
+		leadTransferee:   0,
+		PendingConfIndex: 0,
+		config:           c,
+	}
 }
 
 // sendAppend sends an append RPC with new entries (if any) and the
@@ -183,26 +210,35 @@ func (r *Raft) sendHeartbeat(to uint64) {
 // tick advances the internal logical clock by a single tick.
 func (r *Raft) tick() {
 	// Your Code Here (2A).
+
 }
 
 // becomeFollower transform this peer's state to Follower
 func (r *Raft) becomeFollower(term uint64, lead uint64) {
 	// Your Code Here (2A).
+	r.Term = term
+	r.Lead = lead
+	r.State = StateFollower
 }
 
 // becomeCandidate transform this peer's state to candidate
 func (r *Raft) becomeCandidate() {
 	// Your Code Here (2A).
+	r.Vote = r.id // 给自己投票
+	r.votes[r.id] = true
+	r.State = StateCandidate
 }
 
 // becomeLeader transform this peer's state to leader
 func (r *Raft) becomeLeader() {
 	// Your Code Here (2A).
 	// NOTE: Leader should propose a noop entry on its term
+	r.State = StateLeader
 }
 
 // Step the entrance of handle message, see `MessageType`
 // on `eraftpb.proto` for what msgs should be handled
+// 处理 RPC 消息
 func (r *Raft) Step(m pb.Message) error {
 	// Your Code Here (2A).
 	switch r.State {
@@ -216,11 +252,19 @@ func (r *Raft) Step(m pb.Message) error {
 // handleAppendEntries handle AppendEntries RPC request
 func (r *Raft) handleAppendEntries(m pb.Message) {
 	// Your Code Here (2A).
+	if m.MsgType != pb.MessageType_MsgAppend {
+		return
+	}
+
 }
 
 // handleHeartbeat handle Heartbeat RPC request
 func (r *Raft) handleHeartbeat(m pb.Message) {
 	// Your Code Here (2A).
+	if m.MsgType != pb.MessageType_MsgHeartbeat {
+		return
+	}
+
 }
 
 // handleSnapshot handle Snapshot RPC request
